0b888138bab54978335bb96eddaf4aafb184ec09
Anastasiia Kulachok <kulachokn@gmail.com>
2022-07-24 13:25:59 +0200

    changed task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,105 +1,100 @@
-// import throttle from 'lodash.throttle';
-//
-// const STORAGE_KEY = 'feedback-form-state';
-// const form = document.querySelector('.feedback-form');
-// const formData = {}
-//
-// form.addEventListener('input', throttle(onFormInput, 500));
-// form.addEventListener('submit', onFormSubmit);
-//
-// populateForm();
-//
-// function onFormInput(e) {
-//
-//   const formElValue = e.target.value;
-//   const formElName = e.target.name;
-//   formData[formElName] = formElValue;
-//
-//   const formDataStringified = JSON.stringify(formData);
-//   localStorage.setItem(STORAGE_KEY, formDataStringified);
-// }
-//
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//
-//   if (e.target.email.value === '') {
-//     alert('Please enter your email');
-//     return;
-//   }
-//   if (e.target.message.value === '') {
-//     alert('Please type your message');
-//     return;
-//   }
-//
-//   console.log(formData);
-//
-//   e.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-//
-// function populateForm() {
-//   const storageValue = localStorage.getItem(STORAGE_KEY);
-//   const storageValueParsed = JSON.parse(storageValue);
-//
-//   if (storageValueParsed) {
-//     const formElements = form.elements;
-//     for (const key of storageValueParsed) {
-//       formElements[key].value = storageValueParsed[key];
-//     }
-//   }
-// }
-
 import throttle from 'lodash.throttle';
 
-import localStorageApi from './localstorage';
-
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
+const formData = {}
 
-const feedbackFormState = {};
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
-const fillFormFields = () => {
-  const userDataFromLS = localStorageApi.load('feedback-form-state');
+populateForm();
 
-  if (userDataFromLS === undefined) {
+function onFormInput(e) {
+
+  const formElValue = e.target.value;
+  const formElName = e.target.name;
+  formData[formElName] = formElValue;
+
+  const formDataStringified = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataStringified);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (e.target.email.value === '') {
+    alert('Please enter your email');
+    return;
+  }
+  if (e.target.message.value === '') {
+    alert('Please type your message');
     return;
   }
 
-  const formElements = form.elements;
+  console.log(formData);
 
-  for (const key in userDataFromLS) {
-    if (userDataFromLS.hasOwnProperty(key)) {
-      formElements[key].value = userDataFromLS[key];
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateForm() {
+  const storageValue = localStorage.getItem(STORAGE_KEY);
+
+  const storageValueParsed = JSON.parse(storageValue);
+  console.log(storageValueParsed)
+
+  if (storageValueParsed) {
+    const formElements = form.elements;
+    for (const key of storageValueParsed) {
+      formElements[key].value = storageValueParsed[key];
     }
   }
+}
 
-  const val = {
-    email: formElements.email.value,
-    message: formElements.message.value,
-  };
-
-  console.log(val);
-};
-
-const onFormElChange = event => {
-  const target = event.target;
-
-  const formElVal = target.value;
-  const formElName = target.name;
-
-  feedbackFormState[formElName] = formElVal;
-
-  localStorageApi.save('feedback-form-state', feedbackFormState);
-};
-
-const onFormSubmit = event => {
-  event.preventDefault();
-
-  localStorageApi.remove('feedback-form-state');
-
-  event.currentTarget.reset();
-};
-
-form.addEventListener('input', throttle(onFormElChange, 500));
-fillFormFields();
-
-form.addEventListener('submit', onFormSubmit);
+// import throttle from 'lodash.throttle';
+// import localStorageApi from './localstorage';
+//
+// const form = document.querySelector('.feedback-form');
+//
+// const feedbackFormState = {};
+//
+// const fillFormFields = () => {
+//   const userDataFromLS = localStorageApi.load('feedback-form-state');
+//
+//   if (userDataFromLS === undefined) {
+//     return;
+//   }
+//
+//   const formElements = form.elements;
+//
+//   for (const key in userDataFromLS) {
+//     if (userDataFromLS.hasOwnProperty(key)) {
+//       formElements[key].value = userDataFromLS[key];
+//     }
+//   }
+// };
+//
+// const onFormElChange = event => {
+//   const target = event.target;
+//
+//   const formElVal = target.value;
+//   const formElName = target.name;
+//
+//   feedbackFormState[formElName] = formElVal;
+//
+//   localStorageApi.save('feedback-form-state', feedbackFormState);
+// };
+//
+// const onFormSubmit = event => {
+//   event.preventDefault();
+//
+//   console.log(feedbackFormState);
+//   localStorageApi.remove('feedback-form-state');
+//
+//   event.currentTarget.reset();
+// };
+//
+// fillFormFields();
+//
+// form.addEventListener('input', throttle(onFormElChange, 500));
+// form.addEventListener('submit', onFormSubmit);
